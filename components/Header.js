66c107619b7cff64a1d409d3abd6613351dbcdde
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-05-16 17:01:46 +0530

    added image and text

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,14 @@ export default function Header() {
     <div className="2xl:mx-auto 2xl:container xl:px-0 bg-yellow-400">
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex lg:w-2/5 flex-wrap order-first lg:order-none title-font font-medium items-center justify-center text-green-700 lg:items-center lg:justify-center mb-4 md:mb-0">
+          <a className="flex lg:w-1/3 flex-wrap order-first lg:order-none title-font font-medium items-center justify-center text-green-700 lg:items-center lg:justify-center">
             <span className="ml-3 text-4xl" itemProp="name">
               Sag SabjiWala
             </span>
+            <br />
+            <span className="mt-2 text-sm">Available in BTPS, Kathara, Jarangdih</span>
           </a>
-          <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <div className="lg:w-2/3 inline-flex lg:justify-end ml-5 lg:ml-0">
             <button className="inline-flex items-center bg-green-700 border-0 font-bold py-3 px-6 focus:outline-none hover:bg-green-800 rounded text-yellow-500 mt-4 md:mt-0">
               Order Now
               <svg
--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,17 @@ export default function Home() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-2 mx-auto">
             <h1
-              className="sm:text-3xl text-2xl font-medium title-font text-center text-green-700 mb-20 underline"
+              className="text-xl font-medium title-font text-center text-green-700 mb-20"
               itemProp="description"
             >
-              Fresh Vegetables right at your door - &nbsp;
-              <a href="tel:09931708803" itemProp="telephone">
+              Fresh Green Vegetables, fruits, milk, Egg delivered direct from farm to home. &nbsp;
+              Call/WhatsApp -{' '}
+              <a href="tel:09931708803" itemProp="telephone" className="underline">
                 09931708803
+              </a>
+              /
+              <a href="tel:08271700921" itemProp="telephone" className="underline">
+                08271700921
               </a>
             </h1>
             <div className="flex flex-wrap -m-4" itemScope itemType="https://schema.org/ItemList">
