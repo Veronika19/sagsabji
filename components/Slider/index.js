ea717cfa1ade0ce32ab814ed3aa99b068a94d72a
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-05-17 01:38:31 +0530

    medium deivces slider fine

--- components/Slider/index.js
+++ components/Slider/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
 
 import Image from 'next/image';
 
@@ -10,10 +9,10 @@ function App() {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    let sliderInterval = setInterval(() => setNext(), 3000);
-    return () => {
-      clearInterval(sliderInterval);
-    };
+    // let sliderInterval = setInterval(() => setNext(), 3000);
+    // return () => {
+    //   clearInterval(sliderInterval);
+    // };
   }, [index]);
 
   const setNext = () => {
@@ -29,50 +28,46 @@ function App() {
   };
 
   return (
-    <section className="section">
-      <div className="section-center">
-        {people.map((person, personIndex) => {
-          const { id, image, name, title } = person;
-          console.log(index);
-          let position = personIndex === index ? 'activeSlide' : 'nextSlide';
-          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
-            position = 'lastSlide';
-          }
-          return (
-            <article className={`${position}`} key={id}>
-              <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <Image
-                  width={600}
-                  height={250}
-                  src={image}
-                  alt={name}
-                  className="object-cover object-center rounded"
-                />
-              </div>
-              <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                  {name}
-                  <br className="hidden lg:inline-block" />
-                  {title}
-                </h1>
+    <div className="mt-8 h-20 md:h-80 max-w-screen-xl text-center relative flex overflow-hidden section-center">
+      {people.map((person, personIndex) => {
+        const { id, image, name, title } = person;
+        console.log(index);
+        let position = personIndex === index ? 'activeSlide' : 'nextSlide';
+        if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+          position = 'lastSlide';
+        }
+        return (
+          <article className={`${position} flex flex-row left-6 md:left-8`} key={id}>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+              <Image
+                width={350}
+                height={250}
+                src={image}
+                alt={name}
+                className="object-cover object-center rounded"
+              />
+            </div>
+            <div className="md:-ml-10 lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                {name}
+                <br className="hidden lg:inline-block" />
+                {title}
+              </h1>
 
-                <p class="mb-8 leading-relaxed">
-                  Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
-                  cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage
-                  hot chicken authentic tumeric truffaut hexagon try-hard chambray.
-                </p>
-              </div>
-            </article>
-          );
-        })}
-        <button onClick={() => setPrev()} className="prev">
-          <FiChevronLeft />
-        </button>
-        <button onClick={() => setNext()} className="next">
-          <FiChevronRight />
-        </button>
-      </div>
-    </section>
+              <p className="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
+              </p>
+            </div>
+          </article>
+        );
+      })}
+      <button onClick={() => setPrev()} className="prev bg-yellow-400 text-green-700 rounded-lg">
+        <FiChevronLeft />
+      </button>
+      <button onClick={() => setNext()} className="next bg-yellow-400 text-green-700 rounded-lg">
+        <FiChevronRight />
+      </button>
+    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-2 mx-auto">
             <h1
-              className="text-xl font-medium title-font text-center text-green-700 mb-20"
+              className="text-xl font-medium title-font text-center text-green-700 mb-5"
               itemProp="description"
             >
               Fresh reen Vegetables, Fruits, Milk, Egg delivered directly from farm to home. &nbsp;
