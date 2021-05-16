b668bd07a105c0c79d31653417abf4171458aafd
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-05-17 02:07:32 +0530

    done with slider

--- components/Slider/index.js
+++ components/Slider/index.js
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <div className="mt-8 h-20 md:h-80 max-w-screen-xl text-center relative flex overflow-hidden section-center">
+    <div className="mt-1 mb-8 md:mt-8 h-40 md:h-80 max-w-screen-xl text-center relative flex overflow-hidden section-center">
       {people.map((person, personIndex) => {
         const { id, image, name, title } = person;
         console.log(index);
@@ -37,34 +37,40 @@ function App() {
           position = 'lastSlide';
         }
         return (
-          <article className={`${position} flex flex-row left-6 md:left-8`} key={id}>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <article className={`${position} flex flex-row left-6 md:left-8 items-center`} key={id}>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-2/5 mb-10 md:mb-0">
               <Image
                 width={350}
-                height={250}
+                height={300}
                 src={image}
                 alt={name}
                 className="object-cover object-center rounded"
               />
             </div>
-            <div className="md:-ml-10 lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <div className="-ml-4 md:-ml-10 lg:flex-grow w-3/5 md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="-mt-4 title-font sm:text-4xl md:text-3xl text-lg capitalize mb-4 font-medium text-gray-900">
                 {name}
-                <br className="hidden lg:inline-block" />
+                <br className="inline-block" />
                 {title}
               </h1>
 
-              <p className="mb-8 leading-relaxed">
+              <p className="hidden md:block mb-8 leading-relaxed">
                 Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant
               </p>
             </div>
           </article>
         );
       })}
-      <button onClick={() => setPrev()} className="prev bg-yellow-400 text-green-700 rounded-lg">
+      <button
+        onClick={() => setPrev()}
+        className="top-10 md:top-40 prev bg-yellow-400 text-green-700 rounded-lg"
+      >
         <FiChevronLeft />
       </button>
-      <button onClick={() => setNext()} className="next bg-yellow-400 text-green-700 rounded-lg">
+      <button
+        onClick={() => setNext()}
+        className="top-10 md:top-40 next bg-yellow-400 text-green-700 rounded-lg"
+      >
         <FiChevronRight />
       </button>
     </div>
