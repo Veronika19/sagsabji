a29b9d347c2b600c82e7d0a66ab8791afb9e0239
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-06-05 15:54:02 +0530

    changed yellow-400 to yello-500

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,6 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        yellow: {
-          DEFAULT: '#ffa827',
-          light: '#5c6ac4',
-          dark: '#202e78',
-        },
-      },
-    },
-  },
   variants: {
     extend: {},
   },
