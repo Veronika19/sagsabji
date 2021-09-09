f1c00fcf4d45ee591d8b127f61a843f07cfd939d
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-09-09 17:45:47 +0530

    dues

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ vegetables, fruits, dryFruits }) {
       behavior: 'smooth',
     });
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen invisible">
       <Head>
         <title>Sag SabjiWale</title>
       </Head>
